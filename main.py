aac6083d518bc3af814abc472e67e1dfd16e78e5
francesco.piccolo <francesco.piccolo@lynxspa.com>
2019-10-30 09:44:32 +0100

    Reads a list from the input file, then prints the cards' infos

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import json
-
 # Given a list of numbers in the input.txt file, for each number, print the card's infos.
 path = 'C:\\Users\\francesco.piccolo\\PycharmProjects\\MTGJsonPy\\'
 input = 'input.txt'
+numberList = [line.rstrip('\n') for line in open(path+input)]
 json_input = 'ISD.json'
 json_file = open(path + json_input, encoding="utf8")
 json_set = json.load(json_file)
 json_cards = json_set['cards']
-print(json_cards)
+for targetNumber in numberList:
+    for card in json_cards:
+        if card['number'] == targetNumber:
+            print(card['number']+": "+card['name']+" "+card['manaCost'])
+
+
+
+
